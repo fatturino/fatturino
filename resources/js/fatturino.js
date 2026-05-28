ce35b26bddd81a8f3e39f1685ae3b7d56e2b6427
Daniele Lenares <daniele@lenares.me>
2026-05-28 19:13:41 +0200

    feat: enhance page title formatting in fatturino.js

--- resources/js/fatturino.js
+++ resources/js/fatturino.js
@@ -3,4 +3,22 @@ import { mount } from 'svelte'
 
 createInertiaApp({
     pages: './Pages',
+    title: (title) => {
+        const normalizedTitle = title?.trim();
+        if (normalizedTitle) {
+            return `Fatturino - ${normalizedTitle}`;
+        }
+
+        const pathname = window.location.pathname.replace(/^\/+|\/+$/g, '');
+        const pageName = pathname
+            ? pathname
+                .split('/')
+                .filter(Boolean)
+                .map((segment) => segment.replace(/[-_]+/g, ' '))
+                .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1))
+                .join(' / ')
+            : 'Home';
+
+        return `Fatturino - ${pageName}`;
+    },
 });
