a9e36b00fec5c453161237be3f2389bc1cbed0de
Daniele Lenares <daniele@lenares.me>
2026-06-09 11:52:50 +0200

    feat: add favicon to app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="noindex, nofollow, noarchive" />
         <title inertia>Fatturino</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=outfit:400,500,600,700,800&display=swap" rel="stylesheet">
